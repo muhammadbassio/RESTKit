92297e1b62961812f51bc80e7b9b371168844d9b
Muhammad Bassio <muhammadbassio@gmail.com>
2017-12-28 18:36:50 +0200

    Initial commit

---
+++ RESTKit/RESTKit/RESTKit.h
@@ -0,0 +1,19 @@
+//
+//  RESTKit.h
+//  RESTKit
+//
+//  Created by Muhammad Bassio on 12/28/17.
+//  Copyright © 2017 Muhammad Bassio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RESTKit.
+FOUNDATION_EXPORT double RESTKitVersionNumber;
+
+//! Project version string for RESTKit.
+FOUNDATION_EXPORT const unsigned char RESTKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RESTKit/PublicHeader.h>
+
+
